783b01886bfa199b661fc4de1382916aa31979de
Daniel Yu <20dy3@mammoth.cs.williams.edu>
2020-05-21 14:36:00 -0400

    fixed py script

--- src/vector_generator.py
+++ src/vector_generator.py
@@ -4,5 +4,5 @@ from numpy import savetxt
 data = np.random.uniform(-5.0, 5.0, (5000,2))
 # save to csv file
 savetxt('data.csv', data, delimiter='\t')
-dot = np.dot(data[,0], data[,1])
+dot = np.dot(data[:,0], data[:,1])
 print(dot)
