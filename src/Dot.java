bcf8596b6be2923c1673488a75ad5276b9651072
Daniel Yu <20dy3@mammoth.cs.williams.edu>
2020-05-21 14:30:46 -0400

    dot product init

--- src/Dot.java
+++ src/Dot.java
@@ -13,6 +13,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.Tool;
@@ -34,17 +35,16 @@ import org.apache.hadoop.mapreduce.lib.jobcontrol.JobControl;
 import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
 
 // JSON Simple 
-import org.json.simple.*;
-import org.json.simple.parser.*;
-import org.json.simple.JSONObject;
+//import org.json.simple.*;
+//import org.json.simple.parser.*;
+//import org.json.simple.JSONObject;
 
 /**
  *  MapReduce job chain that 
  * (1) pipes impressions and click logs to produce intermediate ([ referrer, ad_id] , clicked/not clicked) key-val pairs
  * (2) pipes ([ referrer, ad_id] , clicked/not clicked) key-val pairs to produce cumulative [referrer,ad_id], click-through rate
  */
-public class ClickRate {
-
+public class Dot {
     public static void main(String[] args) throws Exception {
 	if (args.length < 2) {
 	    System.err.println("Error: Wrong number of parameters");
@@ -52,145 +52,67 @@ public class ClickRate {
 	    System.exit(1);
 	}
 
-	JobControl jobControl = new JobControl("jobChain"); 
+	Configuration conf = new Configuration();
 
-	// Set up Job 1
-	Configuration conf1 = new Configuration();//getConf();
-	Job job1 = Job.getInstance(conf1, "step 1");
-	job1.setJarByClass(ClickRate.class);
-	job1.setJobName("step1");
-	
+	Job job = Job.getInstance(conf, "trivial job");
+	job.setJarByClass(Dot.class);
+
 	// set the Mapper and Reducer functions we want
-	job1.setMapperClass(ClickRate.FirstMapper.class);
-	job1.setReducerClass(ClickRate.FirstReducer.class);
-	job1.setMapOutputKeyClass(Text.class);
-	job1.setOutputKeyClass(Text.class);
-	
+	job.setMapperClass(Dot.IdentityMapper.class);
+	job.setReducerClass(Dot.IdentityReducer.class);
+	job.setMapOutputKeyClass(DoubleWritable.class);
+	job.setMapOutputValueClass(DoubleWritable.class);
+	job.setOutputKeyClass(DoubleWritable.class);
+	job.setOutputValueClass(DoubleWritable.class);
 	// input arguments tell us where to get/put things in HDFS
-	MultipleInputs.addInputPath(job1, new Path(args[0]), TextInputFormat.class);
-	MultipleInputs.addInputPath(job1, new Path(args[1]), TextInputFormat.class);
-	FileOutputFormat.setOutputPath(job1, new Path("intermediate"));
+	KeyValueTextInputFormat.addInputPath(job, new Path(args[0]));
+	FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-	// Add Job1 to controller
-	ControlledJob controlledJob1 = new ControlledJob(conf1);
-	controlledJob1.setJob(job1);
-	jobControl.addJob(controlledJob1);
-	
-	// Set up Job2
-	Configuration conf2 = new Configuration();
-	Job job2 = Job.getInstance(conf2);
-	job2.setJarByClass(ClickRate.class);
-	job2.setJobName("step 2");
-
-	// set the Mapper and Reducer functions we want
-	job2.setMapperClass(ClickRate.SecondMapper.class);
-	job2.setReducerClass(ClickRate.SecondReducer.class);
-	job2.setMapOutputKeyClass(Text.class);
-	job2.setOutputKeyClass(Text.class);
-
-	// input arguments tell us where to get/put things in HDFS 
-	KeyValueTextInputFormat.addInputPath(job2, new Path("intermediate/part-r-00000"));
-	FileOutputFormat.setOutputPath(job2, new Path(args[2]));
-
-	// Add Job2 to JobControl; chain the two Jobs
-	ControlledJob controlledJob2 = new ControlledJob(conf2);
-	controlledJob2.setJob(job2);
-	controlledJob2.addDependingJob(controlledJob1);
-	jobControl.addJob(controlledJob2);
-
-	// Launch Job Control
-	Thread jobControlThread = new Thread(jobControl);
-	jobControlThread.start();
-	while(!jobControl.allFinished()){}
-	System.exit(job1.waitForCompletion(true) ? 0 : 1);
+	// ternary operator - a compact conditional that just returns 0 or 1
+	System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
     /**
-     * map: (LongWritable, Text) --> (Text, Text)
-     * Reads both logs; for Impressions Log, produce (impression_id, [referrer, ad_id])
-     * For Clicks log, produce (impression_id, click or not)
+     * map: (LongWritable, Text) --> (LongWritable, Text)
+     * NOTE: Keys must implement WritableComparable, values must implement Writable
      */
-    public static class FirstMapper extends Mapper < LongWritable, Text,
-					       Text, Text > {
+    public static class IdentityMapper extends Mapper < LongWritable, Text,
+	DoubleWritable, DoubleWritable > {
+
 	@Override
 	public void map(LongWritable key, Text val, Context context)
 	    throws IOException, InterruptedException {
-	    // Setup JSON Parser
-	    JSONParser parser = new JSONParser();
-	    JSONObject obj = new JSONObject();
-	    try {
-		obj = (JSONObject) parser.parse(val.toString());
-	    } catch (org.json.simple.parser.ParseException e) {
-		System.out.println(e);
-	    }
-	    // Intermediate key,val = impressionId, [referrer, adId] or clicked/xClicked
-	    Text intermediateKey = new Text(obj.get("impressionId").toString());
-	    Text intermediateVal;
-	    if (obj.containsKey("referrer")) {
-		intermediateVal = new Text(("["+obj.get("referrer")+", "+obj.get("adId")+"]"));
-	    } else {
-		intermediateVal = new Text("clicked");
-	    }
-	    // Write to disk
-	    context.write(intermediateKey, intermediateVal);
+	    // write (key, val) out to memory/disk
+	    // uncomment for debugging
+	    //System.out.println("key: "+key+" val: "+val);
+	    String[] vals = val.toString().split("\t");
+	    double product = Double.parseDouble(vals[0]) * Double.parseDouble(vals[1]);	       
+	    //System.out.println(product);
+	    //System.out.println(vals);
+	    context.write(new DoubleWritable(1), new DoubleWritable(product));
 	}
 
     }
 
-    /**                                                                                                                                                             * map: (LongWritable, Text) --> (Text, Text)                                                                                                                   * Reads intermediate file; produce [referrer, adId], click/no click pairs
-     */
-    public static class SecondMapper extends Mapper < LongWritable, Text,
-					       Text, Text > {
-	        @Override
-		public void map(LongWritable key, Text val, Context context)
-		    throws IOException, InterruptedException {
-		    String line = val.toString();
-		    String intermediateKey = line.substring(0, line.length()-1); // referrer + adID
-		    String intermediateVal = line.substring(line.length()-1); // click or not (0 or 1)
-		    context.write(new Text(intermediateKey), new Text(intermediateVal));
-		}
-    }
-    
     /**
-     * reduce: (Text, Text) --> (Text, Text)
-     * Produce intermediate file; for each impression, create [referrer, adId] + 1 or 0 
+     * reduce: (LongWritable, Text) --> (LongWritable, Text)
      */
-    public static class FirstReducer extends Reducer < Text, Text, Text, Text > {
+    public static class IdentityReducer extends Reducer < DoubleWritable, DoubleWritable,
+						DoubleWritable, DoubleWritable > {
+
 	@Override
-	public void reduce(Text key, Iterable < Text > values, Context context)
+	public void reduce(DoubleWritable key, Iterable < DoubleWritable > values, Context context)
 	    throws IOException, InterruptedException {
-	    String finalKey = "Default";
-	    String finalVal = "0";
-	    Iterator<Text> i = values.iterator();
-	    while (i.hasNext()) {
-		String val = i.next().toString();
-	      	if (val.equals("clicked")) {
-		    finalVal = "1";
-		} else {
-		    finalKey = val;
-		}
+	    // write (key, val) for every value
+	    double sum = 0;
+	    for (DoubleWritable val : values) {
+		// uncomment for debugging
+		sum += val.get();
+		//System.out.println("key: "+key+" val: "+val);
 	    }
-	    context.write(new Text(finalKey), new Text(finalVal));
+	    //System.out.println("sum: "+sum);
+	    context.write(key, new DoubleWritable(sum));
 	}
 
-    }
-   /**                                                                                                                                                         
-    * reduce: (Text, Text) --> (Text, Text)                                                                            
-    * Produce final click through rates for each [referrer, adId]
-    */
-    public static class SecondReducer extends Reducer < Text, Text, Text, Text > {
-	        @Override
-		public void reduce(Text key, Iterable < Text > values, Context context)
-		    throws IOException, InterruptedException {
-		    double clicks = 0; // Impression that were clicked
-		    double total = 0; //Total impressions
-		    for (Text val : values) {
-			if (val.toString().equals("1")) {
-			    clicks++;
-			}
-			total++;
-		    }
-		    context.write(key, new Text(Double.toString((clicks/total))));
-		}
     }
 }
---
+++ src/vector_generator.py
@@ -0,0 +1,8 @@
+import numpy as np
+from numpy import asarray
+from numpy import savetxt
+data = np.random.uniform(-5.0, 5.0, (5000,2))
+# save to csv file
+savetxt('data.csv', data, delimiter='\t')
+dot = np.dot(data[,0], data[,1])
+print(dot)
